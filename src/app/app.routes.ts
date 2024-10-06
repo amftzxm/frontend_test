ce4add69b83c77067cc3885467e7657047b30f53
saknarin <saknarin@azurecomputer.com>
2024-10-06 14:14:58 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './module/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', redirectTo: 'login' },
+
+  { path: 'login', component: LoginComponent },
+];
---
+++ src/app/module/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  username: any;
+  password: any;
+}
