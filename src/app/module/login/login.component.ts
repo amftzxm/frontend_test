6ba73b63d3303a971f51753dddad8e052b2a49cb
saknarin <saknarin@azurecomputer.com>
2024-10-06 14:24:33 +0700

    api

--- src/app/module/login/login.component.ts
+++ src/app/module/login/login.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   username: any;
   password: any;
+
+  private http = inject(HttpClient);
+
+
+
+  constructor(){
+    this.http.get<any>('https://official-joke-api.appspot.com/random_joke').subscribe((res)=>{
+      console.log(res);
+
+    })
+  }
 }
