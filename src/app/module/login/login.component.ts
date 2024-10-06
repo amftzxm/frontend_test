ac679637a1a2e34dc158a936c0b08d55b05aba58
saknarin <saknarin@azurecomputer.com>
2024-10-06 14:24:48 +0700

    d

--- src/app/module/login/login.component.ts
+++ src/app/module/login/login.component.ts
@@ -2,12 +2,12 @@ import { Component, inject } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
